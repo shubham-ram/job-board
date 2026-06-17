77bc4bad08861df14a1102bac27c656ddf7962a7
shubham-ram <shubhamram2k@gmail.com>
2026-06-18 01:19:39 +0530

    fix: update expire time of accesstoken

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: "30m",
+    expiresIn: "15m",
   });
 };
 
