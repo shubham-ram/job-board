c0353fa121bd18168df19ff33997d93208db00ee
shubham-ram <shubhamram2k@gmail.com>
2026-06-26 00:33:16 +0530

    feat: update secret key

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, {
+  return jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
     expiresIn: "15m",
   });
 };
 
 export const verifyToken = (token) => {
-  return jwt.verify(token, process.env.JWT_SECRET);
+  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
 };
