772dd0d51934d580ca78c571ed97d8f193bd397c
shubham-ram <shubhamram2k@gmail.com>
2026-06-26 00:58:45 +0530

    fix: improve error handling

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,29 +1,36 @@
+import jwt from "jsonwebtoken";
 import { Account } from "../models/account.model.js";
 import AppError from "../utils/AppError.js";
 import { verifyToken } from "../utils/jwt.js";
 
 async function verifyJWT(req, res, next) {
+  const { accessToken } = req.cookies;
+
+  if (!accessToken) {
+    throw new AppError("Unauthorized request", 401);
+  }
+
+  let decoded;
   try {
-    const { accessToken } = req.cookies;
-
-    if (!accessToken) {
-      throw new AppError("Unauthorized requies", 401);
+    decoded = verifyToken(accessToken);
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      throw new AppError("Access token expired", 401);
     }
-
-    const decoded = verifyToken(accessToken);
-
-    const account = await Account.findById(decoded.accountId);
-
-    if (!account) {
+    if (error instanceof jwt.JsonWebTokenError) {
       throw new AppError("Invalid access token", 401);
     }
-
-    req.account = account;
-
-    next();
-  } catch (error) {
-    throw new AppError(error?.message || "Invalid access token", 401);
+    throw error;
   }
+
+  const account = await Account.findById(decoded.accountId);
+
+  if (!account) {
+    throw new AppError("Invalid access token", 401);
+  }
+
+  req.account = account;
+  next();
 }
 
 export default verifyJWT;
