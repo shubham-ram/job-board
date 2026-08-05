9a98df326a58d40c21ee8f7b8e549a475b12c263
shubham-ram <shubhamram2k@gmail.com>
2026-08-05 22:31:59 +0530

    feat: add uploadVideo utility for handling video uploads to Cloudinary

---
+++ src/utils/uploadVideo.js
@@ -0,0 +1,25 @@
+import streamifier from "streamifier";
+import cloudinary from "../config/cloudinary.js";
+import { RESUME_FOLDER, RESUME_RESOURCE_TYPE } from "../constant.js";
+
+function uploadVideo(file) {
+  return new Promise((res, rej) => {
+    const config = {
+      resource_type: RESUME_RESOURCE_TYPE,
+      folder: RESUME_FOLDER,
+    };
+
+    const cb = (err, result) => {
+      if (err) {
+        rej(err);
+      }
+      res(result);
+    };
+
+    const stream = cloudinary.uploader.upload_stream(config, cb);
+
+    streamifier.createReadStream(file.buffer).pipe(stream);
+  });
+}
+
+export default uploadVideo;
